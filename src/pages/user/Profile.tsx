eab5082eda505bb2b45cfd9b74412194796ebac3
karrot_jin <jin@daangn.com>
2025-11-08 03:52:15 +0900

    fix

--- src/pages/user/Profile.tsx
+++ src/pages/user/Profile.tsx
@@ -1,16 +1,13 @@
 import type { FC } from 'react'
-import { Avatar, Space, Typography, Card, Descriptions } from 'antd'
+import { Avatar, Space, Typography } from 'antd'
 import { UserOutlined, MailOutlined, CalendarOutlined } from '@ant-design/icons'
 import { MainLayout } from '@/components/layout/MainLayout'
-import { PlanEditor } from '@/components/character/PlanEditor'
 import { useAuthStore } from '@/store/authStore'
-import { useMyCharacter } from '@/hooks/queries/useCharacterQuery'
 
 const { Title, Text } = Typography
 
 export const Profile: FC = () => {
   const user = useAuthStore((state) => state.user)
-  const { data: character } = useMyCharacter()
 
   return (
     <MainLayout>
@@ -108,28 +105,6 @@ export const Profile: FC = () => {
             </Space>
           </Space>
         </div>
-
-        {/* Character Info */}
-        {character && (
-          <Card title="내 캐릭터">
-            <Descriptions column={1} bordered>
-              <Descriptions.Item label="캐릭터 이름">{character.name}</Descriptions.Item>
-              <Descriptions.Item label="현재 프롬프트">
-                "{character.current_prompt}"
-              </Descriptions.Item>
-              <Descriptions.Item label="총점">{character.total_score}</Descriptions.Item>
-              <Descriptions.Item label="힘">{character.strength}</Descriptions.Item>
-              <Descriptions.Item label="매력">{character.charm}</Descriptions.Item>
-              <Descriptions.Item label="창의성">{character.creativity}</Descriptions.Item>
-              <Descriptions.Item label="생성일">
-                {new Date(character.created_at).toLocaleDateString('ko-KR')}
-              </Descriptions.Item>
-            </Descriptions>
-          </Card>
-        )}
-
-        {/* Character Plan Editor */}
-        <PlanEditor />
       </Space>
     </MainLayout>
   )
