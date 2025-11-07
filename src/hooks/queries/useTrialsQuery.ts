6f94cfbf53e1082243c16f85b8c3f3d2729394de
con <con@woowahan.com>
2025-11-08 01:35:59 +0900

    trial.service 관련 제거작업

--- src/hooks/queries/useTrialsQuery.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { trialService } from '@/services/trial.service'
-
-export const useActiveRoundTrials = () => {
-  return useQuery({
-    queryKey: ['trials', 'activeRound'],
-    queryFn: () => trialService.getActiveRoundTrials(),
-  })
-}
